7f923091fb929fdfaa4e5f656abfea8371d38521
Marco <modasso23@icloud.com>
2025-04-08 23:04:32 +0200

    Last minor changes to the comments and renaming stuff to make it more understandable

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "Eigen/Core"
 #include "Eigen/LU"
 #include "Eigen/QR"
+
 /* Otherwise, we can include all of the library, uncommenting this next line: */
 //#include "Eigen/Eigen"
 
@@ -15,8 +16,8 @@ using namespace Eigen;
 /* Creating a function that tells us if the relative error of the vectors obtained with the methods used in the code 
 is less than or equal to the machine epsilon. If that's the case, the function returns the boolean value true, 
 otherwise it returns false. We'll use it to print a message that will tell us whether the solution vector and 
-the solution we found are the same vector. */
-bool theyAreTheSame(double const& number){
+the solution we found are the same vector. (not asked in the assignment, but I wanted to try and implement it) */
+bool areTheyTheSame(double const& number){
     double eps = numeric_limits<double>::epsilon();
     if(number <= eps){
         return true;
@@ -37,11 +38,11 @@ int main(){
     /* Let's define the vector solution using the function Ones() for fixed-size matrix types */
     Vector2d x = -Vector2d::Ones();
 
-    /* Finding the solution of each system using the PALU then the QR decomposition with full pivoting*/
+    /* Finding the solution of each system using the PALU then the QR decomposition with full pivoting */
     Vector2d PALU1 = A.fullPivLu().solve(b);
     Vector2d QR1 = A.fullPivHouseholderQr().solve(b);
 
-    /* Calculating the relative error using the lpNorm(2), which is the euclidean norm for vectors 
+    /* Calculating the relative error using the lpNorm(2), which is the euclidean norm for vectors
     or the Frobenius norm for matrixes */
     double rel_err_PALU_1 = ((x - PALU1).norm())/(x.norm());
     double rel_err_QR_1 = ((x - QR1).norm())/(x.norm());
@@ -50,11 +51,11 @@ int main(){
     cout << "The relative error for the 1st system using the PALU decomposition with full pivoting is: " << setprecision(15) 
          << scientific << rel_err_PALU_1 << endl;
     cout << "Therefore, for the computer the vector found using this method is the same as the solution vector x provided: "
-         << boolalpha << theyAreTheSame(rel_err_PALU_1) << endl;
+         << boolalpha << areTheyTheSame(rel_err_PALU_1) << endl;
     cout << "The relative error for the 1st system using the QR decomposition with full pivoting is: " 
          << rel_err_QR_1 << endl;
     cout << "Therefore, for the computer the vector found using this method is the same as the solution vector x provided: "
-         << boolalpha << theyAreTheSame(rel_err_QR_1) << endl << endl;
+         << boolalpha << areTheyTheSame(rel_err_QR_1) << endl << endl;
 
 
     Matrix2d C = A;
@@ -73,13 +74,14 @@ int main(){
     cout << "The relative error for the 2nd system using the PALU decomposition with full pivoting is: " 
          << rel_err_PALU_2 << endl;
     cout << "Therefore, for the computer the vector found using this method is the same as the solution vector x provided: "
-         << boolalpha << theyAreTheSame(rel_err_PALU_2) << endl;
+         << boolalpha << areTheyTheSame(rel_err_PALU_2) << endl;
     cout << "The relative error for the 2nd system using the QR decomposition with full pivoting is: " 
         << rel_err_QR_2 << endl;
     cout << "Therefore, for the computer the vector found using this method is the same as the solution vector x provided: "
-         << boolalpha << theyAreTheSame(rel_err_QR_2) << endl << endl;
+         << boolalpha << areTheyTheSame(rel_err_QR_2) << endl << endl;
 
     Matrix2d E = C;
+    /* The difference between C and E is their second column, so let's modify just that "block" of the matrix */
     E.col(1) << -5.547001955851905e-01,
                 -8.320502947645361e-01;
     Vector2d f;
@@ -94,11 +96,11 @@ int main(){
     cout << "The relative error for the 3rd system using the PALU decomposition with full pivoting is: " << setprecision(15) 
         << scientific << rel_err_PALU_3 << endl;
     cout << "Therefore, for the computer the vector found using this method is the same as the solution vector x provided: "
-         << boolalpha << theyAreTheSame(rel_err_PALU_3) << endl;
+         << boolalpha << areTheyTheSame(rel_err_PALU_3) << endl;
     cout << "The relative error for the 3rd system using the QR decomposition with full pivoting is: " 
         << rel_err_QR_3 << endl;
     cout << "Therefore, for the computer the vector found using this method is the same as the solution vector x provided: "
-        << boolalpha << theyAreTheSame(rel_err_QR_3) << endl << endl;
+        << boolalpha << areTheyTheSame(rel_err_QR_3) << endl << endl;
 
     return 0;
 }
